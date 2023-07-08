d4a47fd4b03956a96f689d135004439cf84dac54
vkkingtong <120380002+vkkingtong@users.noreply.github.com>
2023-07-08 22:59:15 +0800

    创建项目
    创建springboot项目

---
+++ ACMER/src/main/java/com/example/acmer/AcmerApplication.java
@@ -0,0 +1,13 @@
+package com.example.acmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmerApplication.class, args);
+    }
+
+}
