beb2454ec025f9789a9f6277ef709142aa1d4923
jiangyan <you@example.com>
2023-07-19 15:58:36 +0800

    acmer

---
+++ acmer/src/main/java/com/buct/acmer/AcmerApplication.java
@@ -0,0 +1,14 @@
+package com.buct.acmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AcmerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmerApplication.class, args);
+    }
+
+}
