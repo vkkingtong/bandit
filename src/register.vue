433dbc4bd77889ca7cb7c80551b88c4c73d3c482
xiaoTideng <136318103+xiaoTideng@users.noreply.github.com>
2023-07-23 15:35:23 +0800

    Create register.vue

---
+++ src/register.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="register-container">
+    <h2>注册</h2>
+    <form @submit.prevent="register">
+      <label for="username">用户名:</label>
+      <input type="text" id="username" v-model="username" required>
+      <label for="password">密码:</label>
+      <input type="password" id="password" v-model="password" required>
+      <button type="submit">注册</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    register () {
+      alert('用户注册成功！')
+      this.$router.push('/') // 注册成功后跳转到登录页
+    }
+  }
+}
+</script>
+
+<style scoped>
+.register-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-image: url('D:/JAVA/shengchan/coder/src/assets/images/image2.jpg'); /* 设置背景图像的路径 */
+  background-size: cover;
+  background-position: center;
+  //background-color: #f5f5f5;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  margin-bottom: 10px;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  box-sizing: border-box;
+  margin-bottom: 15px;
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  background-color: #409eff;
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #1d8ce0;
+}
+</style>
